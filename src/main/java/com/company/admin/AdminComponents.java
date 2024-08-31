979975325e6acf84a663d85b82eb82eeb7cf9c99
Muhammad Yusuf <myshukurov@gmail.com>
2024-09-01 02:26:39 +0500

    fixed

--- src/main/java/com/company/admin/AdminComponents.java
+++ src/main/java/com/company/admin/AdminComponents.java
@@ -2,20 +2,23 @@ package com.company.admin;
 
 public interface AdminComponents {
 
-    String BAN = "/ban";
-    String UNBAN = "/unban";
     String YOUR_BANNED = "Afsuski Taxi Gruppadan Siz BAN oldingiz. Qayta Qo'shilish uchun adminga murojaat qiling:\n";
     String CAN_BANNED = "Siz 2 kundan kein Taxi gruppadan Ban olishingiz mumkin. Vaqtida admin bilan bog'laning:\n";
     String UNBANNED = "Tabriklaymiz! Siz blokdan chiqarildingiz! Guruhga qo'shilishingiz mumkin";
     String ENTER_THE_GROUP = "Taxi Guruhimizga qo'shiling!";
     String ADD_DURATION = "/add";
     String DURATION_EXTENDED = "Botdan foydalanish muddatingiz uzaytirildi.\nUmumiy: ";
-    String GET = "/get";
     String JOIN_GROUP = "Barcha Klient Malumotlari mana shu guruhga tashlab boriladi. Guruhga qo'shiling \uD83D\uDC47\uD83C\uDFFC\n";
     String SHOULD_PAY = "Botdan Taksi sifatida foydalanish uchun to'lov amalga oshirishingiz kerak! Bunda to'lovdan kein akkauntingiz 30kun davomida faol holatda ishlaydi. Bonus sifatida 5kun qo'shib beriladi!";
-    String SEND = "/send";
-    String CARD = "/card";
     String TAXI_PRICE = "\uD83D\uDCB0 Narx: ";
     String CARD_NUMBER = "\uD83D\uDCB3 Karta Raqami: ";
     String CARD_OWNER = "\uD83E\uDDD1\uD83C\uDFFB\u200D\uD83D\uDCBB Karta Egasi: ";
+
+    //COMMANDS
+    String BAN = "/ban";
+    String UNBAN = "/unban";
+    String CARD = "/card";
+    String SEND = "/send";
+    String GET = "/get";
+    String GET_ALL = "/get-all";
 }
